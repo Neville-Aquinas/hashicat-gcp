2ab3f62ef21ff94ac4efd7c5c86833641eb04326
Neville-Aquinas <neville.aquinas@niveussolutions.com>
2022-06-14 16:14:27 +0000

    Added the second label

--- main.tf
+++ main.tf
@@ -47,9 +47,9 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  labels     = {
+  labels   {
     "department" = "devops",
-    "billable"="true"
+    "billable" = "true"
   }
 
   boot_disk {
